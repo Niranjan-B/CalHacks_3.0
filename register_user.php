7ddb31c27b491a2211ecb32ada3f7cbbcfc4526a
Niranjan B <nero.niranjan@gmail.com>
2016-11-13 14:27:12 -0800

    Update register_user.php

--- register_user.php
+++ register_user.php
@@ -1,7 +1,7 @@
 <?php
         $servername = "localhost";
         $username = "root";
-        $password = "ninja_droid123";
+        $password = "ninja";
 
         $connection = mysql_connect($servername, $username, $password);
 
